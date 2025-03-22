7c602599003a249adb86d0284117702d96175a30
abhittac <abhishekchauhan3003@gmail.com>
2025-03-22 12:24:05 +0530

    react + tailwind

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -5,7 +5,8 @@ import Register from './pages/Register'
 export function App() {
 
   return (
-   
+   <>
     <Register/>
+   </>
   )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,96 @@
 import React from 'react'
 
 export default function Register() {
-    const [name, setName] = useState('');
+  
   return (
-    <div>
-
+    <div className='register'>
+<section id="advertisers" class="advertisers-service-sec pt-5 pb-5">
+  <div class="container">
+    <div class="row">
+      <div class="section-header text-center">
+        <h2 class="fw-bold fs-1 d-flex justify-content-center">
+          
+          <span class="b-class-secondary uppercase">Resgiter </span>Yourself
+        </h2>
+      
+      </div>
+    </div>
+    <div class="row mt-5 mt-md-4 row-cols-1 row-cols-sm-1 row-cols-md-3 justify-content-center">
+      <div class="col">
+        <div class="service-card">
+          <div class="icon-wrapper">
+            <i class="fa-solid fa-chart-line"></i>
+          </div>
+          <h3>Tracking Lead</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Quisquam consequatur necessitatibus eaque.
+          </p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="service-card">
+          <div class="icon-wrapper">
+            <i class="fa-solid fa-arrows-down-to-people"></i>
+          </div>
+          <h3>Advanced Targeting solution</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Quisquam consequatur necessitatibus eaque.
+          </p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="service-card">
+          <div class="icon-wrapper">
+            <i class="fa-solid fa-globe"></i>
+          </div>
+          <h3>Global Reach & Quality Traffic</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Quisquam consequatur necessitatibus eaque.
+          </p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="service-card">
+          <div class="icon-wrapper">
+            <i class="fa-solid fa-money-check-dollar"></i>
+          </div>
+          <h3>Flexible pricing models</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Quisquam consequatur necessitatibus eaque.
+          </p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="service-card">
+          <div class="icon-wrapper">
+            <i class="fa-regular fa-circle-check"></i>
+          </div>
+          <h3>Advanced optimization technologies & methodologies</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Quisquam consequatur necessitatibus eaque.
+          </p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="service-card">
+          <div class="icon-wrapper">
+            <i class="fa-solid fa-people-group"></i>
+          </div>
+          <h3>Dedicated account management team</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Quisquam consequatur necessitatibus eaque.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
 
     </div>
