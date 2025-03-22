2423e61cdd0670539e568ea5fe97f6a11bf2f0b9
abhittac <abhishekchauhan3003@gmail.com>
2025-03-22 12:10:04 +0530

    title

---
+++ client/src/app.jsx
@@ -0,0 +1,11 @@
+
+import './app.css'
+import Register from './pages/Register'
+
+export function App() {
+
+  return (
+   
+    <Register/>
+  )
+}
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Register() {
+    const [name, setName] = useState('');
+  return (
+    <div>
+
+
+
+    </div>
+  )
+}
